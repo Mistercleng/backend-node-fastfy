cfbc6bd19969b054e15f8a63c0119a030b640d9d
mrcleng <mrcleng@gmail.com>
2024-03-29 23:15:20 -0400

    add config eslint

---
+++ src/server.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fastify_1 = require("fastify");
+var app = (0, fastify_1.default)();
+app.get('/hello', function () {
+    return "Hello";
+});
+app.listen({
+    port: 3333,
+}).
+    then(function () {
+    console.log("Http server is running 🚀");
+});
