d8ad2b14b277525606ec52f30b33fb1cb888c164
mrcleng <mrcleng@gmail.com>
2024-03-30 00:52:02 -0400

    add knex and creating first migration

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var fastify_1 = require("fastify");
-var app = (0, fastify_1.default)();
-app.get('/hello', function () {
-    return "Hello";
-});
-app.listen({
+'use strict'
+const __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod }
+  }
+Object.defineProperty(exports, '__esModule', { value: true })
+const fastify_1 = __importDefault(require('fastify'))
+const database_1 = require('./database')
+const app = (0, fastify_1.default)()
+app.get('/hello', async () => {
+  const tables = await (0, database_1.knex)('sqlite_schema').select('*')
+  return tables
+})
+app
+  .listen({
     port: 3333,
-}).
-    then(function () {
-    console.log("Http server is running 🚀");
-});
+  })
+  .then(() => {
+    console.log('Http server is running 🚀')
+  })
