34b2cd0d983242f0c50e2e92a364ca07fc707096
misterclengTrindade <mistercleng.trindade@gaf.energy>
2024-03-31 14:29:07 -0400

    list all transactions and get one transactio

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,12 +4,31 @@ import { knex } from "../database"
 import { randomUUID } from 'node:crypto'
 
 
-export async function transactionsRoutes(app: FastifyInstance){
+export async function transactionsRoutes(app: FastifyInstance): Promise<void>{
+    app.get('/', async () => {
+      const transactions = knex('transactions').select()
+      return transactions 
+    })
+
+    app.get('/:id', async (request)=>{
+      const getTransactionParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const { id } = getTransactionParamsSchema.parse(request.params)
+
+      const transactions = await knex('transactions').where('id',id).first()
+
+      return { transactions }
+
+    })
+
     app.post('/', async (request, reply) => {
         const createTransactionBodySchema = z.object({
           title: z.string(),
           amount: z.number(),
           type: z.enum(['credit','debit'])
+
         })
 
         const {title,amount,type} = createTransactionBodySchema.parse(request.body)
@@ -18,6 +37,8 @@ export async function transactionsRoutes(app: FastifyInstance){
           id: randomUUID(),
           title,
           amount: type === 'credit' ? amount : amount * -1
+          
+
         })
         return reply.status(201).send()
       })
